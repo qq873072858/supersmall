90a6ee0b7e761d6f06c3294e2b0f37d8d2112fb4
黎松润 <873072858@qq.com>
2022-04-10 09:54:30 +0800

    no message

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': 'src',
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+        'router': '@/router',
+        'store': '@/store'
+      }
+    }
+  }
+}
