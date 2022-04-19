853e093ab682ac54321ffb3349a757b1b23e6945
黎松润 <873072858@qq.com>
2022-04-19 22:46:41 +0800

    no message

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,37 +1,47 @@
 <template>
   <div class="goods">
-    <goods-list-item v-for="item in goods" :key="item.title" :goodsItem="item">
+    <goods-list-item v-for="item in goods" :key="item.guid=guid()" :goodsItem="item">
+
     </goods-list-item>
   </div>
 </template>
 
 <script>
-  import GoodsListItem from './GoodsListItem.vue'
+import GoodsListItem from "./GoodsListItem.vue";
 
-  export default {
-    name: 'GoodsList',
-    components: {
-      GoodsListItem
-    },
-    props: {
-      goods: {
-        type: Array,
-        default() {
-          return []
-        }
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
       }
-    },
-    mounted() {
-      console.log(this.goods)
+    }
+  },
+  mounted() {
+    console.log(this.goods);
+  },
+  methods: {
+    guid() {
+      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
     }
   }
+};
 </script>
 
 <style scoped>
-  .goods {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    padding: 2px;
-  }
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 2px;
+}
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,13 +4,20 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" 
-      :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
+
+    <scroll class="content" ref="scroll" 
+            :probe-type="3" :pull-up-load="true"
+            @scroll="contentScroll" @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" 
+        :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
     
+    <back-top @click.native="backClick()" v-show="isShowBackTop"></back-top>
+
   </div>
 </template>
 
@@ -22,8 +29,11 @@
   import NavBar from 'components/common/navbar/NavBar.vue'
   import TabControl from 'components/content/tabControl/TabControl.vue'
   import GoodsList from 'components/content/goods/GoodsList.vue'
+  import Scroll from 'components/common/scroll/Scroll.vue'
+  import BackTop from 'components/content/backTop/BackTop.vue'
 
   import { getHomeMultidata, getHomeGoods } from 'network/home'
+import { clearTimeout, setTimeout } from 'timers';
 
   export default {
     name: "Home",
@@ -33,7 +43,9 @@
       RecommendView,
       FeatureView,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll,
+      BackTop
     },
     data() {
       return {
@@ -44,7 +56,8 @@
           'new': { page: 0, list: [] },
           'sell': { page: 0, list: [] }
         },
-        currentType: 'pop'
+        currentType: 'pop',
+        isShowBackTop: false
       }
     },
     computed: {
@@ -52,14 +65,21 @@
         return this.goods[this.currentType].list;
       }
     },
-    created() {
-      this.getHomeMultidata()
-
-      this.getHomeGoods('pop')
-      this.getHomeGoods('new')
-      this.getHomeGoods('sell')
-    },
     methods: {
+      /**
+       * 防抖函数
+       */
+      debounce(func, delay){
+        let timer = null
+        return function(...args) {
+          if(timer) clearTimeout(timer)
+          
+          timer = setTimeout(() => {
+            func.apply(this, args)
+          }, delay)
+        }
+      },
+
       /**
        * 事件监听相关方法
        */
@@ -75,7 +95,15 @@
             this.currentType = 'sell'
             break
         }
-        console.log(index)
+      },
+      backClick() {
+        this.$refs.scroll.scrollTo(0, 0, 500);
+      },
+      contentScroll(position) {
+        this.isShowBackTop = -position.y > 1000
+      },
+      loadMore() {
+        this.getHomeGoods(this.currentType)
       },
 
       /**
@@ -94,15 +122,44 @@
         // 2.请求商品数据
         getHomeGoods(type, page).then(res => {
           this.goods[type].list.push(...res.data.data.list)
+          this.goods[type].page += 1
+
+          // 完成上拉加载更多
+          this.$refs.scroll.finishPullUp()
         })
       }
+    },
+    created() {
+
+      // 利用promise保证必须请求完数据才能渲染
+      new Promise(() => {
+        this.getHomeMultidata()
+
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')
+      });
+
+    },
+    mounted() {
+
+      const refreshFun = this.debounce(this.$refs.scroll.refresh, 300)
+
+      //监听item中图片加载成功
+      this.$bus.$on('itemImageLoad', () => {
+
+        refreshFun();
+
+        //console.log('------------')
+        //this.$refs.scroll.refresh()
+      })
     }
   }
 </script>
 
 <style scoped>
   #home {
-    padding-top: 44px;
+    height: 100vh;
   }
 
   .home-nav {
@@ -121,5 +178,11 @@
     position: sticky;
     top: 44px;
     z-index: 9;
+  }
+
+  .content {
+    height: calc(100vh - 49px - 40px);
+    margin-top: 44px;
+    /* height: 500px; */
   }
 </style>
